1c098ca10d3898b24a97a6a1688fd05d696655a2
Pawel <31793531+PriestOfAdanos@users.noreply.github.com>
2021-01-12 23:12:11 +0100

    Add files via upload

---
+++ skrt_kol.py
@@ -0,0 +1,28 @@
+st = "ala ma kota ala ala ala"
+
+def my_split(st):
+    word=""
+    for ch in st:
+        if ch==" ":
+            yield word
+            word = ""
+            continue
+        word+=ch
+    
+
+def word_count(arr):
+    dct = dict()
+    for word in arr:
+        print(word)
+        if word in dct.keys():
+            dct[word]+=1
+        else:
+            dct[word]=1
+    return dct
+
+
+
+print(word_count(my_split(st)))
+
+
+
